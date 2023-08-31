c10c29d7c287164132e5776c0f0b95ccb3629234
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-08-31 12:53:05 -0400

    npcs

--- src/main/java/me/kotayka/mbc/NPCs/NPC.java
+++ src/main/java/me/kotayka/mbc/NPCs/NPC.java
@@ -1,49 +1,117 @@
 package me.kotayka.mbc.NPCs;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import me.kotayka.mbc.Participant;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityHeadRotation;
+import net.minecraft.network.protocol.game.PacketPlayOutNamedEntitySpawn;
+import net.minecraft.network.protocol.game.PacketPlayOutPlayerInfo;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.EntityPlayer;
 import net.minecraft.server.level.PlayerInteractManager;
 import net.minecraft.server.level.WorldServer;
+import net.minecraft.server.network.PlayerConnection;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_18_R2.CraftServer;
 import org.bukkit.craftbukkit.v1_18_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class NPC {
 
     private JavaPlugin plugin;
-    private UUID uuid = UUID.randomUUID();
-
-    private String name;
-    private String playerName;
-    private String skin;
     private Location location;
+    private Player player;
+    private List<Player> viewers = new ArrayList<>();
+    private EntityPlayer npc;
+    private int entityID;
 
-    private List<Participant> viewers = new ArrayList<>();
-
-    public NPC(JavaPlugin plugin, String name, String playerName, String skin, Location loc) {
+    public NPC(JavaPlugin plugin, Player player, Location loc) {
         this.plugin = plugin;
-        this.name = name;
-        this.playerName = playerName;
-        this.skin = skin;
+        this.player = player;
         this.location = loc;
+    }
 
-        MinecraftServer minecraftServer = ((CraftServer) Bukkit.getServer()).getServer();
-        WorldServer worldServer = ((CraftWorld) this.location.getWorld()).getHandle();
-        GameProfile gameProfile = new GameProfile(this.uuid, this.playerName);
+    public int create() {
+        MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer();
+        WorldServer world = ((CraftWorld) Objects.requireNonNull((player).getWorld())).getHandle();
 
-        gameProfile.getProperties().put("textures", new Property("textures", "textures", "signature"));
+        String[] things = getRequest(player.getName());
 
+        String texture = things[0];
+        String signature = things[1];
 
-        EntityPlayer npc = new EntityPlayer(minecraftServer, worldServer, gameProfile);
+        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), "NPC");
+
+        gameProfile.getProperties().put("textures", new Property("textures", texture, signature));
+
+        EntityPlayer npcPlayer = new EntityPlayer(server, world, gameProfile);
+        Location npcLocation = this.location;
+        npcPlayer.b(npcLocation.getX(), npcLocation.getY(), npcLocation.getZ(), npcLocation.getYaw(), npcLocation.getPitch());
+
+        this.entityID = npcPlayer.getBukkitEntity().getEntityId();
+        this.npc = npcPlayer;
+
+        return this.entityID;
+    }
+
+    public void show(Player p) {
+        PlayerConnection connection = ((CraftPlayer) p).getHandle().b;
+        connection.a(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.a, this.npc));
+        connection.a(new PacketPlayOutNamedEntitySpawn(this.npc));
+        connection.a(new PacketPlayOutEntityHeadRotation(this.npc, (byte) (this.location.getYaw() * 256 / 360)));
+
+        EntityPlayer npc = this.npc;
+
+        viewers.add(p);
+
+        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+            public void run() {
+                connection.a(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.e, npc));
+            }
+        }, 20);
+    }
+
+    public void remove(Player p) {
+        PlayerConnection connection = ((CraftPlayer) p).getHandle().b;
+        connection.a(new PacketPlayOutEntityDestroy(this.npc.ae()));
+
+        if (this.viewers.contains(p)) {
+            this.viewers.remove(p);
+        }
+    }
+
+    public String[] getRequest(String name)  {
+        try {
+            URL url_0 = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
+            InputStreamReader reader_0 = new InputStreamReader(url_0.openStream());
+            String uuid = new JsonParser().parse(reader_0).getAsJsonObject().get("id").getAsString();
+
+            URL url_1 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
+            InputStreamReader reader_1 = new InputStreamReader(url_1.openStream());
+            JsonObject textureProperty = new JsonParser().parse(reader_1).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
+            String texture = textureProperty.get("value").getAsString();
+            String signature = textureProperty.get("signature").getAsString();
+
+            return new String[] {texture, signature};
+        } catch (IOException e) {
+            System.err.println("Could not get skin data from session servers!");
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
--- src/main/java/me/kotayka/mbc/NPCs/NPCManager.java
+++ src/main/java/me/kotayka/mbc/NPCs/NPCManager.java
@@ -1,18 +1,72 @@
 package me.kotayka.mbc.NPCs;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityHeadRotation;
+import net.minecraft.network.protocol.game.PacketPlayOutNamedEntitySpawn;
+import net.minecraft.network.protocol.game.PacketPlayOutPlayerInfo;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.EntityPlayer;
 import net.minecraft.server.level.WorldServer;
+import net.minecraft.server.network.PlayerConnection;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_18_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_18_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.*;
 
 public class NPCManager {
-    public NPCManager() {
+    public final JavaPlugin plugin;
+    public Map<Integer, NPC> npcs = new HashMap<>();
 
-
-
+    public NPCManager(JavaPlugin plugin) {
+        this.plugin = plugin;
     }
 
-    public void getServer() {
-        this.mine
+    public int createNPC(Player player, Location loc){
+        NPC npc = new NPC(this.plugin, player, loc);
+        int entityID = npc.create();
+        npcs.put(entityID, npc);
+        return entityID;
+    }
+
+    public void show(int id, Player p) {
+        if (npcs.containsKey(id)) {
+            npcs.get(id).show(p);
+        }
+    }
+
+    public void showAll(int id) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            this.show(id, p);
+        }
+    }
+
+    public void addPlayer(Player p) {
+        for (Map.Entry<Integer, NPC> entry : npcs.entrySet()) {
+            entry.getValue().show(p);
+        }
+    }
+
+    public void remove(int id, Player p) {
+        if (npcs.containsKey(id)) {
+            npcs.get(id).show(p);
+        }
+    }
+
+    public void removeAll(int id) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            this.remove(id, p);
+        }
     }
 }
