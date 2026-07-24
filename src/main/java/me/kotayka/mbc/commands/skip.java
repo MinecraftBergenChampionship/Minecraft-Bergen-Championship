9cb8a462dc611c44559a48b151138241e0106f5b
rspacerr <61034617+rspacerr@users.noreply.github.com>
2026-07-24 15:44:39 -0400

    update: allow skip use in prod with confirmation

--- src/main/java/me/kotayka/mbc/commands/skip.java
+++ src/main/java/me/kotayka/mbc/commands/skip.java
@@ -15,10 +15,28 @@ public class skip implements CommandExecutor {
             return false;
         }
 
-        if (sender.isOp() && (!MBC.getInstance().logStats() || !(MBC.getInstance().getMinigame() instanceof Lobby))) {
+        if (!sender.isOp()) {
+            sender.sendMessage("This is an admin only command!");
+            return false;
+        }
+
+        if (MBC.getInstance().getMinigame().timeRemaining <= 0) return false;
+
+        if (MBC.getInstance().logStats() && (args.length != 1 || !args[0].equalsIgnoreCase("confirm"))) {
+            sender.sendMessage("Please do /skip confirm if event is ongoing!");
+            return false;
+        }
+
+        if (MBC.getInstance().logStats() && args.length == 1 && args[0].equalsIgnoreCase("confirm")) {
             MBC.getInstance().getMinigame().timeRemaining = 1;
             return true;
         }
+
+        if ((!MBC.getInstance().logStats() || !(MBC.getInstance().getMinigame() instanceof Lobby))) {
+            MBC.getInstance().getMinigame().timeRemaining = 1;
+            return true;
+        }
+
 
         sender.sendMessage("This is a debug only command!");
         return false;
